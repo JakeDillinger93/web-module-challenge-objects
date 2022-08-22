4ce9d716e9e4810518f72e047fa904ed05f88789
Jake Richards <jake.riichards93@gmail.com>
2022-08-21 18:14:29 -0700

    finished 1st task

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ The function should:
 */
 
 
-function createMenuItem(/*Your code here*/){
-  /*Your code here*/
+function createMenuItem(name, price, category){
+  const tacos = {name: "tacos", price: 8, category: "Lunch"};
+  return createMenuItem;
 }
 
 
